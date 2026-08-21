4c383a590aff4d671370f35b82362d4ccd2457d7
Mr-Felipe <veranofelipe28@gmail.com>
2026-08-20 21:27:15 -0500

    feat: proyecto completo Mochi Postres - angular, tailwind, supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://iakkcxsjhuiaykyajbca.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlha2tjeHNqaHVpYXlreWFqYmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzOTU2MzQsImV4cCI6MjEwMTk3MTYzNH0.NNLQGE_DcfmTLfmbdbOrrLF8HkJ3hPJlfrxPMGaTJ0o'
+};
